e0dca5e8b49823ab20a5022ce62092dbc6649664
@ontheroad_jp <dev@ontheroad.jp>
2016-09-03 23:56:18 +0900

    [Fix] UID and GID settings

--- 3.2.0-alpine/docker-entrypoint-ex.sh
+++ 3.2.0-alpine/docker-entrypoint-ex.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+set -e
 
 user="redis"
 group="redis"
 
-if [ -z "${TOYBOX_GID}" ] && ! cat /etc/group | grep ":${TOYBOX_GID}:" > /dev/null 2>&1; then
+if [ -n "${TOYBOX_GID}" ] && ! cat /etc/passwd | awk 'BEGIN{ FS= ":" }{ print $4 }' | grep ${TOYBOX_GID} > /dev/null 2>&1; then
     groupmod -g ${TOYBOX_GID} ${group}
     echo "GID of ${group} has been changed."
 fi
 
-if [ -z "${TOYBOX_UID}" ] && ! cat /etc/passwd | grep ":${TOYBOX_UID}:" > /dev/null 2>&1; then
+if [ -n "${TOYBOX_UID}" ] && ! cat /etc/passwd | awk 'BEGIN{ FS= ":" }{ print $3 }' | grep ${TOYBOX_UID} > /dev/null 2>&1; then
     usermod -u ${TOYBOX_UID} ${user}
     echo "UID of ${user} has been changed."
 fi
